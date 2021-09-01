aea5f959b1af013dea3d37f76634f45cad1697ed
Roman <slyusarrv@yandex.ru>
2021-09-01 15:33:02 +0500

    complite task 2 lesson 1

--- lesson 01 - Introduction to Algorithmization and Implementation of Simple Algorithms in Python/02. task 2.py
+++ lesson 01 - Introduction to Algorithmization and Implementation of Simple Algorithms in Python/02. task 2.py
@@ -4,4 +4,34 @@
 Объяснить полученный результат.
 """
 
-print(bin(5), bin(6))
+a = 5
+b = 6
+
+a_byte = format(a, 'b')
+b_byte = format(b, 'b')
+
+rez = []
+
+print("Логическое \"И\": ")
+for i in range(len(a_byte)):
+    rez.insert(i, int(a_byte[i]) and int(b_byte[i]))
+
+print("Результат: ", ''.join(str(e) for e in rez))
+
+rez = []
+
+print("Логическое \"ИЛИ\": ")
+for i in range(len(a_byte)):
+    rez.insert(i, int(a_byte[i]) or int(b_byte[i]))
+
+print("Результат: ", ''.join(str(e) for e in rez))
+
+
+print("Побитовый сдвиг числа 5 на два знака вправо: ")
+# Отрезаем у скиска два знака с конца и дописываем два нуля вначале
+list_to_right = [0, 0] + list(a_byte)[0:-2]
+print(''.join(str(e) for e in list_to_right))
+
+print("Побитовый сдвиг числа 5 на два знака влево: ")
+list_to_left = list(a_byte)[2:len(a_byte)] + [0, 0]
+print(''.join(str(e) for e in list_to_left))
