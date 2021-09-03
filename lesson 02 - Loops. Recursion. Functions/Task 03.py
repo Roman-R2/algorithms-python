22c2f870d83d1200646d1a870ff98654026096bc
Roman <slyusarrv@yandex.ru>
2021-09-03 11:02:25 +0500

    task 3 complite

--- lesson 02 - Loops. Recursion. Functions/Task 03.py
+++ lesson 02 - Loops. Recursion. Functions/Task 03.py
@@ -4,17 +4,13 @@
 """
 
 try:
-    number = int(input("Введите первое число: "))
+    number = int(input("Введите натуральное число: "))
 
     str_int = str(number)
 
-    for i in str_int:
-        if int(i) % 2 == 0:
-            even_digits_count += 1
-        else:
-            odd_digits_count += 1
-
-    print(f"В этом числе {even_digits_count} четных цифр и {odd_digits_count} нечетных цифр.")
+    print("Перевернули: ", end='')
+    for elem in reversed(str_int):
+        print(elem, end='')
 
 except ValueError:
     print("Вы ввели не натуральное число!")
