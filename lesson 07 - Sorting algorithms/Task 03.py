d49329449804c958fbaef416fb47519250b4d57e
Roman <slyusarrv@yandex.ru>
2021-09-21 16:00:38 +0500

    Update Task 03.py

--- lesson 07 - Sorting algorithms/Task 03.py
+++ lesson 07 - Sorting algorithms/Task 03.py
@@ -4,3 +4,23 @@
 которые не меньше медианы, в другой – не больше медианы. Задачу можно решить без сортировки исходного массива.
 Но если это слишком сложно, то используйте метод сортировки, который не рассматривался на уроках
 """
+import random
+from statistics import median
+
+if __name__ == '__main__':
+    # Сгенерируем список с нечетным колличеством элементов, лучше бы кортеж, но ксловия задачим такие
+    emulate_list = [random.randrange(0, 10) for _ in range(1, 10)]
+    print(f"Исходный список: {emulate_list}")
+    print(f"Для проверки: {median(emulate_list)}")
+
+    # Скопируем в список, чтобы не изменить исходный
+    list_for_search = emulate_list.copy()
+
+    left_part = []
+    middle = len(list_for_search) // 2 + 1
+    for i in range(middle):
+        get_min = list_for_search.pop(list_for_search.index(min(list_for_search)))
+        if i == middle - 1:
+            print(f"Медиана: {get_min}. Левая упорядоченная часть {left_part}")
+            break
+        left_part += [get_min]
