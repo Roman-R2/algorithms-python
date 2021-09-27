5e6b207aa30e9ddb4b488755d3fcd779f63ec6e8
Roman <slyusarrv@yandex.ru>
2021-09-27 12:41:17 +0500

    some edits

--- lesson 08 - Trees. Hash function/task_02_huffman_algorithm.py
+++ lesson 08 - Trees. Hash function/task_02_huffman_algorithm.py
@@ -7,10 +7,10 @@ from collections import Counter
 # В данном случае узел - это элемент, у которого всегля есть левый и правий узел или лист
 class HuffmanNode:
     def __init__(self, left, right, frequency):
-        self.data = 'not_leaf'
-        self.frequency = frequency
         self.left = left
         self.right = right
+        self.frequency = frequency
+        self.data = 'not_leaf'
 
 
 class HuffmanAlgorithm:
@@ -20,6 +20,7 @@ class HuffmanAlgorithm:
         else:
             self.string = string
             self.get_code_table()
+            self.code_dict = {}
 
     def error(self, message) -> None:
         print(f"------------------------\n{self.__class__} : {message}\n------------------------\n")
@@ -30,6 +31,11 @@ class HuffmanAlgorithm:
         return char_frequency
 
     def sort_frequency_dict(self, frequency):
+        """
+        Функция сортирует словарь по значению в порядке убывания
+        :param frequency: словарь для сортировки
+        :return: отсортированный по убыванию словарь
+        """
         return dict(sorted(frequency.items(), key=lambda item: item[1], reverse=True))
 
     def create_bst(self):
@@ -43,32 +49,35 @@ class HuffmanAlgorithm:
         while len(frequency) > 1:
             last_1 = frequency.popitem()
             last_2 = frequency.popitem()
-            huffman_node = HuffmanNode(last_1, last_2, (last_1[1] + last_2[1]))
-            frequency.update({huffman_node: (last_1[1] + last_2[1])})
+            huffman_node = HuffmanNode(last_2, last_1, (last_1[1] + last_2[1]))
+            frequency.update({huffman_node: (last_2[1] + last_1[1])})
             frequency = self.sort_frequency_dict(frequency)
         return huffman_node
 
     def get_code_table(self):
         huffman_bsd = self.create_bst()
+        print(huffman_bsd.left[0])
         if not huffman_bsd:
             return None
         else:
-            if huffman_bsd.data == 'not_leaf':
-                if isinstance(huffman_bsd.left, tuple):
-                    print(huffman_bsd.left[0], 0)
-                else:
-                    pass
-                if isinstance(huffman_bsd.right, tuple):
-                    print(huffman_bsd.right[0], 1)
-                else:
-                    pass
+            code_table = self._get_code_table(huffman_bsd, "0", "1")
+        print(code_table)
+        return code_table
 
-    def _get_code_table(self, ):
-        pass
+    def _get_code_table(self, huffman_bsd, l_code: str, r_code: str):
+        if isinstance(huffman_bsd.left[0], HuffmanNode):
+            self._get_code_table(huffman_bsd.left[0], l_code+"0", r_code+"0")
+        else:
+            self.code_dict.update({huffman_bsd.left[0]: l_code})
+        if isinstance(huffman_bsd.right[0], HuffmanNode):
+            self._get_code_table(huffman_bsd.right[0], f"{l_code}1", f"{r_code}1")
+        else:
+            self.code_dict.update({huffman_bsd.left[0]: r_code})
+        return self.code_dict
 
 
 if __name__ == '__main__':
-    message = "bee"
+    message = "beep boop beer!"
 
     huffman = HuffmanAlgorithm(message)
 
